0e57881c67a21ca4d90532e115194032fa665547
StepIgor <stepanow.igor@gmail.com>
2021-09-21 22:57:32 +0300

    project list element basis is implemented

--- src/ProjectList.js
+++ src/ProjectList.js
@@ -1,21 +1,51 @@
 import "./ProjectList.css"
 import {useState} from "react";
+import {projects, langs} from "./db";
+import Slide from "react-reveal/Slide"
+import Fade from "react-reveal/Fade";
 
-function ProjectList(props){
+function ProjectList(props) {
 
     const [showFrontend, editShowFrontend] = useState(true)
 
-    return (
-        <div className={`external-block`}>
-            <div className={`tab-menu`}>
-                <div onClick={() => editShowFrontend(true)} className={`indigo-text tab-btn ${showFrontend ? `btn-active` : ``}`}>
-                    Frontend
+    let plist
+
+    plist = Object.keys(projects).filter((p) => projects[p].frontend == showFrontend).map((pj) => {
+        return (
+            <Fade key={pj.toString()}>
+                <div className={`plist-element`}>
+                    <div className={`plist-element-picture`}>
+                        <img src={'icons/' + projects[pj].icon}/>
+                    </div>
+                    <div className={`plist-element-body`}>
+                        <div className={`indigo-text`}>{projects[pj].name}</div>
+                        <div>{projects[pj].short_description}</div>
+                        <div>{projects[pj].langs.map((lg) => <span key={lg.toString()}
+                            className={langs[lg].color}> {langs[lg].name}</span>)}</div>
+                    </div>
                 </div>
-                <div onClick={() => editShowFrontend(false)} className={`indigo-text tab-btn ${!showFrontend ? `btn-active` : ``}`}>
-                    Другое
+            </Fade>
+        )
+    })
+
+    return (
+        <Slide bottom>
+            <div className={`external-block`}>
+                <div className={`tab-menu`}>
+                    <div onClick={() => editShowFrontend(true)}
+                         className={`indigo-text tab-btn ${showFrontend ? `btn-active` : ``}`}>
+                        Frontend
+                    </div>
+                    <div onClick={() => editShowFrontend(false)}
+                         className={`indigo-text tab-btn ${!showFrontend ? `btn-active` : ``}`}>
+                        Другое
+                    </div>
+                </div>
+                <div className={`plist-container`}>
+                    {plist}
                 </div>
             </div>
-        </div>
+        </Slide>
     )
 }
 
---
+++ src/db.js
@@ -0,0 +1,27 @@
+export let projects = {
+    0: {
+        frontend : true,
+        name : 'PhotoStory',
+        short_description : 'Сервис публикации пользовательских изображений',
+        langs : [0, 1],
+        icon : 'photostory.png'
+    },
+    1: {
+        frontend : true,
+        name : 'Видео',
+        short_description : 'Сервис публикации пользовательских видеозаписей',
+        langs : [0, 1],
+        icon : 'video.png'
+    }
+}
+
+export let langs = {
+    0 : {
+        name : 'Svelte',
+        color : 'deep-orange-text text-accent-3'
+    },
+    1 : {
+        name : 'JavaScript',
+        color : 'yellow-text text-accent-2'
+    }
+}
