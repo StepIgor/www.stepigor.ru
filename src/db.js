3564cd7117b98746228b736848505c72534aea7f
StepIgor <stepanow.igor@gmail.com>
2021-09-24 16:53:48 +0300

    Release v.1

--- src/db.js
+++ src/db.js
@@ -22,6 +22,30 @@ export let projects = {
         git : 'https://github.com/StepIgor/video.difres.ru',
         screenshots : ['video.difres.ru_(HD).png', 'video.difres.ru_(HD) (1).png', 'video.difres.ru_(HD) (2).png', 'video.difres.ru_(HD) (3).png', 'video.difres.ru_(HD) (4).png', 'video.difres.ru_(HD) (5).png'],
         features : ['Система подписок', 'Система лайков', 'Поисковая система', 'Адаптивность', 'История просмотров', 'Рекомендации']
+    },
+    2: {
+        frontend : true,
+        name : 'Дневник',
+        short_description : 'Ведение собственных записей по предметам',
+        langs : [2, 1],
+        icon : 'diary.png',
+        active : true,
+        url : 'https://diary.difres.ru',
+        git : 'https://github.com/StepIgor/diary.difres.ru',
+        screenshots : ['diary.difres.ru.png', 'diary.difres.ru (1).png', 'diary.difres.ru (2).png', 'diary.difres.ru (3).png'],
+        features : ['Свои предметы', 'Свои записи', 'Смена статуса', 'Адаптивность']
+    },
+    3: {
+        frontend : true,
+        name : 'Сайт разработчика',
+        short_description : 'Представление всех собственных проектов',
+        langs : [2, 1],
+        icon : 'stepigor.png',
+        active : true,
+        url : 'https://www.stepigor.ru',
+        git : 'https://github.com/StepIgor/www.stepigor.ru',
+        screenshots : ['stepigor.ru.png', 'stepigor.ru (1).png', 'stepigor.ru (2).png'],
+        features : ['Представление всех проектов', 'Контактные сведения', 'Скриншоты и описание к работе']
     }
 }
 
@@ -31,5 +55,8 @@ export let langs = {
     },
     1 : {
         name : 'JavaScript'
+    },
+    2 : {
+        name : 'React'
     }
 }
